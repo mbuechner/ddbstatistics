6c8e948abddaba35477c136fb267039a21be7c39
Michael Büchner <m.buechner@dnb.de>
2023-11-07 14:26:08 +0100

    Implementation with Select2

--- src/main/java/de/ddb/dashboard/Application.java
+++ src/main/java/de/ddb/dashboard/Application.java
@@ -1,6 +1,8 @@
 package de.ddb.dashboard;
 
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +16,25 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Slf4j
 public class Application {
 
+    private OkHttpClient httpClient; // http client
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
     public OkHttpClient httpClient() {
-        return new OkHttpClient.Builder().build();
+        if (httpClient != null) {
+            return httpClient;
+        }
+        final Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(128);
+        dispatcher.setMaxRequestsPerHost(64);
+        httpClient = new OkHttpClient.Builder()
+                .connectTimeout(0, TimeUnit.SECONDS)
+                .readTimeout(0, TimeUnit.SECONDS)
+                .dispatcher(dispatcher)
+                .build();
+        return httpClient;
     }
 }
